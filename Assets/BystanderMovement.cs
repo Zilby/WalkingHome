fa566013520359a27535c8325a16be9927f3da53
bravo-c <bravo.c@husky.neu.edu>
2017-04-18 00:58:02 -0400

    nothing works

---
+++ Assets/BystanderMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BystanderMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.x >= 0) {
+			gameObject.transform.position = new Vector2 (gameObject.transform.position.x - 1.0f, gameObject.transform.position.y);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+}
