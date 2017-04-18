3b686106970ed55dae2b37956e43c0f5137e1a0e
Rachel Ellis <rachellis@Rachels-MacBook-Pro.local>
2017-04-18 18:13:46 -0400

    removed some old scripts from the scripts directory

--- Assets/Scripts/VisibilityController.cs
+++ Assets/Scripts/VisibilityController.cs
@@ -2,27 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// outdated script to control visibility of pieces of the maze via cloud sprites that would cover various areas. TODO
+// script used to destroy a GameObject that has a trigger Collider2D component
 public class VisibilityController : MonoBehaviour {
-
-	GameObject cloud;
-	GameObject hero;
-	Collider2D heroColl;
-
-	[SerializeField]
-	private Collider2D cloudColl;
-
-	// Use this for initialization
-	void Start () {
-		cloud = this.gameObject;
-		hero = GameObject.FindGameObjectWithTag ("hero");
-		heroColl = hero.GetComponent<Collider2D> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (cloudColl.IsTouching (heroColl)) {
-			Destroy (cloud);
-		}
+	void OnTriggerEnter2D(Collider2D col) {
+		Destroy (gameObject);
 	}
 }
