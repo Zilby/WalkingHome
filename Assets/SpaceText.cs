33c5e07b980c616289f0a58d58366b4af137f806
Rachel Ellis <rachellis@Rachels-MacBook-Pro.local>
2017-04-19 02:30:27 -0400

    stuff

---
+++ Assets/SpaceText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SpaceText : MonoBehaviour {
+
+	public List<string> endWords;
+	public Text spacePrompt;
+	int wordIndex;
+
+	// Use this for initialization
+	void Start () {
+		wordIndex = 1;
+		GetComponent<Text> ().text = endWords [0];
+		spacePrompt.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (wordIndex == 2) {
+			spacePrompt.enabled = false;
+		}
+
+		if (wordIndex >= endWords.Count + 1) {
+			if (SceneManager.GetActiveScene().name.Equals("OpenerText")) {
+				SceneManager.LoadSceneAsync ("Scroller");
+			} else if (SceneManager.GetActiveScene().name.Equals("CreditsTransition")) {
+				SceneManager.LoadSceneAsync ("Credits");
+			}
+		}
+
+		if (Input.GetKeyDown("space") && wordIndex < endWords.Count) {
+			GetComponent<Text> ().text = endWords [wordIndex];
+			wordIndex += 1;
+		} else if (Input.GetKeyDown ("space")) {
+			wordIndex += 1;
+		}
+	}
+}
