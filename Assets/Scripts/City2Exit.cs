9c54a5007991f19908a9fd9fa46aaa8031ea9287
Zilby <azilby@gmail.com>
2017-04-18 23:48:34 -0400

    added transitions

---
+++ Assets/Scripts/City2Exit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class City2Exit : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "hero") {
+			SceneManager.LoadScene ("EndScrollerTransition");
+		}
+	}
+}
---
+++ Assets/Scripts/Maze2Exit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Maze2Exit : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "hero") {
+			SceneManager.LoadScene ("City2Transition");
+		}
+	}
+}
